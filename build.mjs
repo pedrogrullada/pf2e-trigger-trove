955697d4ab0d0b8a9332bacb66d48c51d4aa8dd1
Chasa <79132112+ChasarooniZ@users.noreply.github.com>
2025-11-12 11:00:16 -0600

    Update build.mjs
    Co-authored-by: pedrogrullada <103401186+pedrogrullada@users.noreply.github.com>

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import { compilePack } from "@foundryvtt/foundryvtt-cli";
 import { existsSync } from "fs";
 import fs from "fs/promises";
 import path from "path";
-import { combineTriggers } from "./scripts/combine-triggers";
+import { combineTriggers } from "./scripts/combine-triggers.js";
 
 // Clean output directory, or create build directory
 const outDir = path.resolve(process.cwd(), "build");
