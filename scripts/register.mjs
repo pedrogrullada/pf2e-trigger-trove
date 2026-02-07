6b722dcf419ee9d7b2e0e27c47dd2377ab62f7df
pedrogrullada <pedrogrullada@gmail.com>
2026-02-07 13:25:15 -0300

    Look for system-specific triggers in the triggers folder

--- scripts/register.mjs
+++ scripts/register.mjs
@@ -1,4 +1,4 @@
 Hooks.once("triggerEngine.registerTriggers", (registerTriggers) => {
   registerTriggers("trigger-engine", "pf2e-trigger", "modules/pf2e-trigger-trove/triggers/base-triggers.json");
-  registerTriggers("trigger-engine", "pf2e-trigger", `modules/pf2e-trigger-trove/${game.system.id}-triggers.json`);
+  registerTriggers("trigger-engine", "pf2e-trigger", `modules/pf2e-trigger-trove/triggers/${game.system.id}-triggers.json`);
 });
